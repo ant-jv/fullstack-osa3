7e3f9e7647ed75ef2fe621200c5eb1970f79851c
Antti Viljanen <viljasenantti@gmail.com>
2022-07-10 12:09:11 +0300

    testing

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ app.get('/info', (request, response) => {
     Person.find().then( people => {
         response.type('text/plain')
         const message = 'Phonebook has info for ' + people.length + ' people.\n' + Date()
-        response.send(message)
+        response.send(people)
     })
 })
 
