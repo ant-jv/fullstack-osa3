7249e1c113eb1cc0a903fae09647b02a22e39dcd
Antti Viljanen <viljasenantti@gmail.com>
2022-07-10 15:01:53 +0300

    uibuild

--- index.js
+++ index.js
@@ -27,37 +27,24 @@ app.use(morgan(function (tokens, req, res) {
     ].join(' ')
   }))
 
-let persons = [
-    {
-      "name": "Testi Testinen",
-      "number": "1234",
-      "id": 1
-    },
-    {
-        "name": "Esimerkki Esimerkkinen",
-        "number": "1234",
-        "id": 2
-    }
-]
-
-
-
 app.get('/api/persons', (request, response) => {
     Person.find().then( people => {
         response.json(people)
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id).then(person => {
         response.json(person)
     })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndDelete(request.params.id).then(() => {
         response.status(204).end()
     })
+    .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
@@ -96,8 +83,26 @@ app.post('/api/persons', (request, response) => {
 
 })
 
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+  
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
